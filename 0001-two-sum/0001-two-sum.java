180c133feb39484aa5016a2cdcaaf59b45021082
GauriPatare <gauripatare1302@gmail.com>
2026-08-10 22:16:24 +0530

    Time: 8 ms (46.88%), Space: 47 MB (74.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,50 @@
+import java.util.Arrays;
+
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        for (int i=0;i<nums.length;i++){
-            for (int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[] {i,j};
-                }
+
+    public int[] twoSum(int[] arr, int target) {
+
+        int n = arr.length;
+
+        // [value, original index]
+        int[][] numsWithIndex = new int[n][2];
+
+        // Store value and original index
+        for (int i = 0; i < n; i++) {
+            numsWithIndex[i][0] = arr[i];
+            numsWithIndex[i][1] = i;
+        }
+
+        // Sort by value
+        Arrays.sort(numsWithIndex,
+                (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Two pointers
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+
+            int sum = numsWithIndex[left][0]
+                    + numsWithIndex[right][0];
+
+            if (sum == target) {
+
+                return new int[] {
+                    numsWithIndex[left][1],
+                    numsWithIndex[right][1]
+                };
+
+            } else if (sum < target) {
+
+                left++;
+
+            } else {
+
+                right--;
             }
         }
-        return new int[]{};
+
+        return new int[] {-1, -1};
     }
 }
