8c7257eabefe5dc9f5fa07e66679223942c8b873
GauriPatare <gauripatare1302@gmail.com>
2026-08-11 23:09:20 +0530

    Time: 1 ms (77.27%), Space: 46.7 MB (43.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,34 +1,40 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
+        
+   
+        // If array is empty, return 0
+        if (nums.length == 0) return 0;
 
-        int i=0;
+        // Pointer for last unique element
+        int i = 0;
 
-        for(int j=1;j<nums.length;j++){
-            if(nums[j]!=nums[i]){
+        // Start from second element
+        for (int j = 1; j < nums.length; j++) {
+            // If new unique element is found
+            if (nums[j] != nums[i]) {
+                // Move unique position forward
                 i++;
-                nums[i]=nums[j];
+                // Place new unique element
+                nums[i] = nums[j];
             }
         }
-        return i+1;
 
-
-        
+        // i is last index of unique element, count = i + 1
+        return i + 1;
     }
 }
 
-public class main{
-    public static void main(String[]args){
-        int [] nums={1,1,2};
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
 
-        Solution sol=new Solution();
-        int k=sol.removeDuplicates(nums);
-        System.out.println("unique count"+k);
-        System.out.print("Array after remove duplicates:");
-        for(int x=0;x<k;x++){
-            System.out.print(nums[x]+"");
+        Solution sol = new Solution();
+        int k = sol.removeDuplicates(nums);
+
+        System.out.println("Unique count = " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int x = 0; x < k; x++) {
+            System.out.print(nums[x] + " ");
         }
     }
 }
