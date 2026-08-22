418f37a972583a9749fdd143c19116e1df4675ef
GauriPatare <gauripatare1302@gmail.com>
2026-08-22 22:21:27 +0530

    Time: 0 ms (100%), Space: 45 MB (12.36%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,31 @@
+class Solution {
+   
+    public int searchInsert(int[] arr, int x) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = n; 
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 6};
+        int x = 5;
+
+        Solution obj = new Solution();
+        int index = obj.searchInsert(arr, x);
+
+        System.out.println("The index is: " + index);
+    }
+}
